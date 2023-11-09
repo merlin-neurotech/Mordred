bbfe66522def331cc12d2b27bfd3260a0fd9b036
ScottMcHaffie <117526656+ScottMcHaffie@users.noreply.github.com>
2023-11-08 22:42:19 -0500

    Adding Neurol Package Files

---
+++ neurol/BCI_tools.py
@@ -0,0 +1,234 @@
+'''
+Module including utility functions for creating `classifier`'s,
+`transfromer`'s, and `calibrator`'s for use in the `BCI` module.
+'''
+
+import time
+
+try:
+    import biosppy.signals as bsig
+except ImportError:
+    raise ImportError(
+        "biosppy is not installed. \n"
+        "biosppy is required for some functionality in BCI_tools.\n"
+        "you can install it using `pip install biosppy`")
+
+import numpy as np
+
+from .models.classification_tools import get_channels
+from .models.preprocessing import epoch, epoch_band_features
+
+# is this right? is it 220 Hz (see documentation)?
+DEVICE_SAMPLING_RATE = {'muse': 256, 'unicorn':250,
+                        }
+
+# FIXME: go through and fix
+# what to do w/ DEVICE_SAMPLING_RATE dict
+# `transformers` not being used
+# the use of `device`
+def ensemble_transform(signal, epoch_len=None, channels=None, device=None,
+                        transformers=None, filter_=False, sampling_rate=None,
+                        filter_kwargs=None):
+    '''
+    Ensemble transform function. Takes in buffer as input. Extracts the
+    appropriate channels and samples, performs filtering, and transforms.
+
+    Arguments:
+        signal(np.ndarray): signal of shape: [n_samples, n_channels]
+        epoch_len(int): length of epoch expected by classifier (# of samples).
+                        optional.
+        channels(list of str or int): list of channels expected by classifier.
+                        See get_channels. optional.
+        device(str): device name. used to get channels and sampling_rate.
+        filter_(boolean): whether to perform filtering
+        filter_kwargs(dict): dictionary of kwargs passed to filtering function.
+            See biosppy.signals.tools.filter_signal. by default,
+            an order 8 bandpass butter filter is performed between 2Hz and 40Hz.
+    '''
+
+    transformed_signal = signal
+
+    # get the latest epoch from the signal
+    if epoch_len is not None:
+        transformed_signal = np.array(signal[-1*epoch_len:, :])
+
+    # get the selected channels
+    if channels:
+        transformed_signal = get_channels(transformed_signal, channels, device)
+
+    # filter_signal
+    if filter_:
+        if not sampling_rate:
+            try:
+                sampling_rate = DEVICE_SAMPLING_RATE[device]
+            except KeyError:
+                raise ValueError(
+                    'sampling_rate is required when filter_ is True.\n'
+                    'Note: this can sometimes be extracted from '
+                    'the `device` parameter for supported devices')
+        else:
+            if not filter_kwargs:
+                filter_kwargs = {}
+            transformed_signal = filter_signal(
+                transformed_signal, sampling_rate, **filter_kwargs)
+
+
+    # apply pipeline of transformers
+    if transformers:
+        for transformer in transformers:
+            transformed_signal = transformer(transformed_signal)
+
+    return transformed_signal
+
+
+def filter_signal(signal, sampling_rate, ftype='butter', band='bandpass',
+                  frequency=(2, 40), order=8, **filter_kwargs):
+    """
+    applies frequency-based filters to a given signal.
+
+    Args:
+        signal (np.ndarray): signal of shape [n_samples, n_channels]
+        sampling_rate(float): sampling rate of signal.
+        ftype (str, optional): type of filter.
+            one of 'FIR', 'butter', 'chebby1', 'chebby2', 'ellip', or 'bessel'.
+            Defaults to 'butter'.
+        band (str, optional): band type.
+            one of 'lowpass', 'highpass', 'bandpass', or 'bandstop'.
+            Defaults to 'bandpass'.
+        frequency (float or tuple of floats, optional): cutoff frequencies.
+            single if 'lowpass'/'highpass', tuple if 'bandpass'/'bandstop'.
+            Defaults to (2,40).
+        order (int, optional): order of filter. Defaults to 8.
+        **filter_kwargs: keyword args for biosppy.signals.tools.filter_signal
+
+    Returns:
+        [np.ndarray]: filtered signal
+    """
+
+    filtered, _, _ = bsig.tools.filter_signal(
+        signal=signal.T, sampling_rate=sampling_rate, ftype=ftype, band=band,
+        frequency=frequency, order=order, **filter_kwargs)
+    filtered = filtered.T
+
+    return filtered
+
+
+def band_power_calibrator(stream, channels, device, bands, percentile=50,
+                          recording_length=10, epoch_len=1,
+                          inter_window_interval=0.2):
+    '''
+    Calibrator for `generic_BCI.BCI` which computes a given `percentile` for
+    the power of each frequency band across epochs channel-wise. Useful for
+    calibrating a concentration-based BCI.
+
+    Arguments:
+        stream(neurol.streams object): neurol stream for brain data.
+        channels: array of strings with the names of channels to use.
+        device(str): device name for use by `classification_tools`
+        bands: the frequency bands to get power features for.
+            'all': all of ['theta', 'alpha_low', 'alpha_high', 'beta', 'gamma']
+            otherwise an array of strings of the desired bands.
+        percentile: the percentile of power distribution across epochs to
+            return for each band.
+        recording_length(float): length of recording to use for calibration
+            in seconds.
+        epoch_len(float): the length of each epoch in seconds.
+        inter_window_interval(float): interval between each window/epoch
+            in seconds (measured start to start)
+
+    Returns:
+        clb_info: array of shape [n_bands, n_channels] of the `percentile`
+        of the power of each band
+    '''
+    sampling_rate = DEVICE_SAMPLING_RATE[device]  # get sampling_rate
+    # calculate window size in # of samples
+    ws = int(epoch_len * sampling_rate)
+    # calculate inter_window_interval in # of samples
+    iwi = int(inter_window_interval * sampling_rate)
+
+    input('Press Enter to begin calibration...')
+
+    print(f'Recording for {recording_length} seconds...')
+
+    # sleep for recording_length while stream accumulates data
+    # necessary so that no data is used before the indicated start of recording
+    time.sleep(recording_length)
+    recording = stream.get_data()  # get accumulated data
+    # get appropriate channels
+    recording = get_channels(np.array(recording), channels, device=device)
+
+    # epoch the recording to compute percentile across distribution
+    epochs = epoch(recording, ws, iwi)
+
+    # compute band power for each epoch
+    band_power = np.array([epoch_band_features(epoch, sampling_rate,
+                                               bands=bands, return_dict=False)
+                           for epoch in epochs])
+
+    # calculate given percentile of band power
+    clb_info = np.squeeze(np.percentile(band_power, percentile, axis=0))
+
+    print(f'\nComputed the following power percentiles: \n{clb_info}')
+
+    return clb_info
+
+
+def band_power_transformer(buffer, clb_info, channels, device, bands,
+                           epoch_len=1):
+    '''
+    Transformer for `generic_BCI.BCI` which chooses channels, epochs, and
+    gets power features on some choice of bands.
+
+    Arguments:
+        buffer: most recent stream data. shape: [n_samples, n_channels]
+        clb_info: not used. included for compatibility with generic_BCI.BCI
+        channels: list of strings of the channels to use.
+        device:(str): device name for use by `classification_tools`.
+        bands: the frequency bands to get power features for.
+            'all': all of ['theta', 'alpha_low', 'alpha_high', 'beta', 'gamma']
+            otherwise a list of strings of the desired bands.
+        epoch_len(float): the duration of data to classify on in seconds.
+
+    Returns:
+        transformed_signal: array of shape [n_bands, n_channels] of the
+        channel-wise power of each band over the epoch.
+    '''
+    sr = DEVICE_SAMPLING_RATE[device]  # get device sampling rate
+
+    # get the latest epoch_len samples from the buffer
+    transformed_signal = np.array(buffer[-int(epoch_len*sr):, :])
+
+    # get the selected channels
+    transformed_signal = get_channels(transformed_signal, channels, device)
+
+    # compute band_features on signal
+    transformed_signal = np.squeeze(epoch_band_features(
+        transformed_signal, sr, bands=bands, return_dict=False))
+
+    return transformed_signal
+
+# def band_power_transformer(signal, sampling_rate, bands): 
+#     '''
+#     Transformer for `generic_BCI.BCI` which chooses channels, epochs, and
+#     gets power features on some choice of bands.
+
+#     Arguments:
+#         signal(np.ndarray): most recent stream data.
+#             shape: [n_samples, n_channels]
+#         sampling_rate(float): sampling_rate of signal. CHANGED THIS TO BE DEVICE, can now get sampling rate from the dictionary
+#         bands: the frequency bands to get power features for.
+#             'all': all of ['theta', 'alpha_low', 'alpha_high', 'beta', 'gamma']
+#             otherwise a list of strings of the desired bands.
+
+#     Returns:
+#         transformed_signal: array of shape [n_bands, n_channels] of the
+#         channel-wise power of each band over the epoch.
+#     '''
+#     # sampling_rate = DEVICE_SAMPLING_RATE[device]
+
+#     # compute band_features on signal
+#     transformed_signal = np.squeeze(epoch_band_features(
+#         signal, sampling_rate, bands=bands, return_dict=False))
+    
+
+#     return transformed_signal
---
+++ neurol/models/classification_tools.py
@@ -0,0 +1,131 @@
+'''
+Module containing functions for performing classification, via machine learning
+models or otherwise, related to Brain-Computer Interface applications.
+'''
+
+import numpy as np
+
+
+
+def get_channels(signal, channels, device=None):
+    '''
+    Returns a signal with only the desired channels.
+
+    Arguments:
+        signal(np.ndarray): a signal of shape [n_samples, n_channels]
+        channels(array): str names or int indices of the desired channels.
+            returned in given order.
+        device(str): name of the device. Optional.
+
+    Returns:
+        numpy array of signal with shape [n_channels, n_desired_channels].
+        Includes only the selected channels in the order given.
+    '''
+
+    # check device; each device has its own ch_ind dictionary corresponding to
+    # its available channels
+    if isinstance(channels[0], str):
+        if device == 'muse':
+            ch_ind_muse = {'TP9': 0, 'AF7': 1, 'AF8': 2, 'TP10': 3}
+            return_signal = np.array([np.array(signal)[:, ch_ind_muse[ch]]
+                                    for ch in channels]).T
+        elif device == 'unicorn':
+            ch_ind_muse = {'EEG 1': 0, 'EEG 2': 1, 'EEG 3': 2, 'EEG 4': 3,'EEG 5': 4, 'EEG 6': 5, 'EEG 7': 6, 'EEG 8': 7}
+            #print(len(signal[0]))
+            #print(channels)
+            return_signal = np.array([np.array(signal)[:, ch_ind_muse[ch]] for ch in channels]).T
+            #print(str(len(return_signal)) + 'by ' + str(len(return_signal[0])))
+        
+        else:
+            raise ValueError(
+                'Given device is not supported. '
+                'You should extract the desired channels manually.')
+
+    elif isinstance(channels[0], int):
+        return_signal = np.array([np.array(signal)[:, ch]
+                                    for ch in channels]).T
+
+    else:
+        raise ValueError('Invalid channel type. Must be str name or int index.')
+
+    return return_signal
+
+
+def softmax_predict(input_, predictor, thresh=0.5):
+    '''
+    Consolidates a softmax prediction to a one-hot encoded prediction.
+
+    Arguments:
+        input_: the input taken by the predictor
+        predictor: function which returns a softmax prediction given an input_
+        thresh: the threshold for a positive prediction for a particular class.
+
+    '''
+
+    pred = np.array(predictor(input_))
+
+    return (pred >= thresh).astype(int)
+
+
+def encode_ohe_prediction(prediction):
+    '''
+    Returns the index number of the positive class in a
+    one-hot encoded prediction.
+    '''
+    return np.where(np.array(prediction) == 1)[0][0]
+
+
+def decode_prediction(prediction, decode_dict):
+    '''
+    Returns a more intelligible reading of the prediction
+    based on the given decode_dict
+    '''
+    return decode_dict[prediction]
+
+
+def threshold_clf(features, threshold, clf_consolidator='any'):
+    '''
+      Classifies given features based on a given threshold.
+
+      Arguments:
+          features: an array of numerical features to classify
+          threshold: threshold for classification. A single number, or an
+            array corresponding to `features` for element-wise comparison.
+          clf_consolidator: method of consolidating element-wise comparisons
+            with threshold into a single classification.
+              'any': positive class if any features passes the threshold
+              'all': positive class only if all features pass threshold
+              'sum': a count of the number of features which pass the threshold
+              function: a custom function which takes in an array of booleans
+                and returns a consolidated classification
+
+      Returns:
+          classification for the given features. Return type `clf_consolidator`.
+    '''
+    try:
+        label = np.array(features) > np.array(threshold)
+    except ValueError as v_err:
+        print("Couldn't perform comparison between features and thresholds."
+              "Try a different format for the threshold.")
+        raise v_err
+
+    # consolidate binary label array into single classification
+    if clf_consolidator == 'any':
+        label = np.any(label)
+
+    elif clf_consolidator == 'all':
+        label = np.all(label)
+
+    elif clf_consolidator == 'sum':
+        label = np.sum(label)
+
+    elif callable(clf_consolidator):
+        try:
+            label = clf_consolidator(label)
+        except TypeError as t_err:
+            print("Couldn't consolidate classification with `clf_consolidator`")
+            raise t_err
+    else:
+        raise ValueError('The given clf_consolidator is not supported.')
+
+    return label
