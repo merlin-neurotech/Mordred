7fcaa4ee064c72827bf622f1b2e4d26c6db32d75
ScottMcHaffie <scottpmchaffie@gmail.com>
2023-11-21 10:02:25 -0500

    Update MusicSuggestions.py
    Figure out how the clf function works, and are trying to run for more than band.

--- MusicSuggestions.py
+++ MusicSuggestions.py
@@ -11,11 +11,17 @@ import matplotlib.image as mpimg
 import numpy as np
 import time
 
-clb = lambda stream:  BCI_tools.band_power_calibrator(stream, ['EEG 1', 'EEG 2', 'EEG 3', 'EEG 4', 'EEG 5', 'EEG 6', 'EEG 7', 'EEG 8'], 'unicorn', bands=['beta'],
-                                                        percentile=5, recording_length=10, epoch_len=1, inter_window_interval=0.25)
 
-gen_tfrm = lambda buffer, clb_info: BCI_tools.band_power_transformer(buffer, clb_info, ['EEG 1', 'EEG 2', 'EEG 3', 'EEG 4', 'EEG 5', 'EEG 6', 'EEG 7', 'EEG 8'], 'unicorn', bands=['beta'],
+print("Enter the calibration period (in seconds).")
+calib_time = input()
+
+
+clb = lambda stream:  BCI_tools.band_power_calibrator(stream, ['EEG 1', 'EEG 2', 'EEG 3', 'EEG 4', 'EEG 5', 'EEG 6', 'EEG 7', 'EEG 8'], 'unicorn', bands=['beta', 'alpha_low'],
+                                                        percentile=5, recording_length=int(calib_time), epoch_len=1, inter_window_interval=0.25)
+
+gen_tfrm = lambda buffer, clb_info: BCI_tools.band_power_transformer(buffer, clb_info, ['EEG 1', 'EEG 2', 'EEG 3', 'EEG 4', 'EEG 5', 'EEG 6', 'EEG 7', 'EEG 8'], 'unicorn', bands=['beta', 'alpha_low'],
                                                         epoch_len=1)
+
 
 global xs, EEG1, EEG2, EEG3, EEG4, EEG5, EEG6, EEG7, EEG8                                                   
 xs = []
@@ -29,18 +35,18 @@ EEG7 = []
 EEG8 = []
 
 def clf(clf_input, clb_info):
+    print ("clb info: ", clb_info)
 
     clf_input = clf_input[:clb_info.shape[0]]
 
-    #EEG1.append(clf_input[0])
-    EEG1.append(clf_input[0])
-    EEG2.append(clf_input[1])
-    EEG3.append(clf_input[2])
-    EEG4.append(clf_input[3])
-    EEG5.append(clf_input[4])
-    EEG6.append(clf_input[5])
-    EEG7.append(clf_input[6])
-    EEG8.append(clf_input[7])
+    EEG1.append(clf_input[0][0])
+    EEG2.append(clf_input[0][1])
+    EEG3.append(clf_input[0][2])
+    EEG4.append(clf_input[0][3])
+    EEG5.append(clf_input[0][4])
+    EEG6.append(clf_input[0][5])
+    EEG7.append(clf_input[0][6])
+    EEG8.append(clf_input[0][7])
     xs.append(len(EEG1))
 
     binary_label = classification_tools.threshold_clf(clf_input, clb_info, clf_consolidator='all')
