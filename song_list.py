af5fd9722e51f2bfabd907e9bf55b92e522edfe0
ScottMcHaffie <scottpmchaffie@gmail.com>
2023-11-28 10:07:07 -0500

    Added mp3 functionality.

--- song_list.py
+++ song_list.py
@@ -1,21 +1,33 @@
+import playsound
+import random
 
+def find_song(wave_flag):
+    # for alpha waves
+    if (wave_flag == True):
+        playsound()
 
 feelings_to_songs = {
     #Theta waves 
-    "Lethargic": "Happy - Pharrell Williams",
-    "Awake": "Someone Like You - Adele",
+    "Lethargic": ["Happy - Pharrell Williams"],
+    "Awake": ["Someone Like You - Adele"],
     #Alpha waves
-    "Focused": ["Can't Stop the Feeling - Justin Timberlake", "Pursuit of Happiness Remix- Kid Cudi"],
-    "Not Focused": "Weightless - Marconi Union",
+    "Focused": ["screamsaver_subtronics", "thelastgoodbye_odesza"],
+    "NotFocused": ["Weightless - Marconi Union"],
     #Beta waves
     #Classical
-    "Calm": "Break Stuff - Limp Bizkit",
+    "Calm": ["Break Stuff - Limp Bizkit"],
     #EDM
-    "Pumped": "Summer of '69 - Bryan Adams"
+    "Pumped": ["Summer of '69 - Bryan Adams"],
     #Gamma waves
-    "Transcendant": "Interstellar - Hans Zimmer?",
-    "Disoriented": "Creepy Music"
+    "Transcendant": ["Interstellar - Hans Zimmer?"],
+    "Disoriented": ["Creepy Music"]
 }
 
-print(feelings_to_songs["Excited"][1])
+n = random.randint(0, len(feelings_to_songs["Focused"]) - 1)
+print (feelings_to_songs["Focused"][n])
+playsound.playsound("Focused/"+feelings_to_songs["Focused"][n]+".mp3", True)
+# print (feelings_to_songs["Focused"])
+# feelings_to_songs["Focused"].pop(0)
+# print (feelings_to_songs["Focused"])
+
 
